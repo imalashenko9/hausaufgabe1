2c18ca9a89bda6f41a57e52772144bf4faa8a89f
irynamalashenko <missis.malashenko@gmail.com>
2022-03-27 18:43:14 +0200

    hausaufgabe2

---
+++ hausaufgabe 2.py
@@ -0,0 +1,61 @@
+#1
+def count_integer(numbers, integer):
+    count = 0
+    for i in numbers:
+        if (i == integer):
+            count += 1
+    if (count == 0):
+            count = 42
+    return count
+numbers = [6, 8, 10, 8, 20, 10, 8, 8]
+integer = 8
+print(count_integer(numbers, integer))
+
+
+#2
+def list_fun(numbers, integer):
+    count = 0
+    n = numbers.copy()
+
+    for i in range(0, len(numbers)):
+        if n[i] == integer:
+            n[i] = 6
+            count += 1
+    n3 = n.copy()
+    n3.reverse()
+    print(n3)
+    if count == 0:
+        return []
+    else:
+        return n
+
+numbers = [6, 10, 8, 20, 10, 8, 8]
+integer = 8
+print(list_fun(numbers, integer))
+
+
+#3
+
+def compare_lists(list1,list2):
+    compare = []
+    for i in list1:
+        if i in list2 and i not in compare:
+            compare.append(i)
+    return [] if not compare else sorted(compare)
+
+list1 = [6, 20, 10]
+list2 = [7, 8, 8]
+d = compare_lists (list1,list2)
+print(d)
+
+#4
+def remove_duplicates(lst):
+    res = []
+    for i in lst:
+        if i not in res:
+            res.append(i)
+    return res
+
+lst = [6, 10, 8, 20]
+d = remove_duplicates(lst)
+print(d)
