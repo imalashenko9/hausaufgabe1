8dfa63a2b3d56252ad429205309136195cd6fb50
irynamalashenko <missis.malashenko@gmail.com>
2022-04-04 22:58:06 +0200

    hausaufgabe3

--- main.py
+++ main.py
@@ -1,82 +1,106 @@
+
 #1
-def name_age(name,age):
-    name = str(input("Hello "))
-    age = int(input("Your age is: "))
-    result = (name + str(age))
-    return result
-print (name_age("",""))
+def count_vowels(text):
+    vowels= 0
+    if not (type(text) == str):
+        return 42
+
+    for i in text:
+        if i in "aeiouAEIOU":
+            vowels = vowels+1
+    return vowels
+
+text = ["kfkf","kfkf"]
+print(count_vowels(text))
 
 #2
-def swap_integers(num1, num2):
-    return num2,num1
-num1 = 10
-num2 = 22
-print('Before swapping:')
-print(f'num1 = {num1} @', id(num1))
-print(f'num2 = {num2} @', id(num2))
-num1, num2 = swap_integers(num1,num2)
-print('After swapping:')
-print(f'num1 = {num1} @', id(num1))
-print(f'num2 = {num2} @', id(num2))
-print(num1,num2, sep="")
+def hamming(text1,text2):
+
+    hamming_distance = 0
+    for i, j in zip(text1,text2):
+        if i != j: hamming_distance += 1
+    if len(text1) == len(text2):
+       return int(hamming_distance)
+    else:
+        return 0
+
+text1 = "cat"
+text2 = "kaa"
+print(hamming(text1,text2))
 
 #3
-def check_numbers(num1,num2):
-     if (num1 or num2) % 6 and (num1 and num2) % 10:
-         return True
-     else:
-         return False
-i = check_numbers(6, 10)
-print(i)
+class Vehicle:
+    def __init__(self, color, fuel_type):
+        self.color = color
+        self.fuel_type = fuel_type
+
+class Car(Vehicle):
+    def __init__(self, color, fuel_type,doors):
+        super().__init__(color, fuel_type)
+        self.doors = doors
+    def __str__(self):
+        return "Color: {0}, Fuel Type: {1}, Doors: {2}".format(self.color,self.fuel_type,self.doors)
+class Bus(Vehicle):
+    def __init__(self, color, fuel_type, passengers):
+        super().__init__(color, fuel_type)
+        self.passengers = passengers
+    def __str__(self):
+        return "Color: {0}, Fuel Type: {1}, Passengers: {2}".format(self.color,self.fuel_type,self.passengers)
+
+car1 = Car("<color>", "<fuel_type>", "<doors>")
+bus1 = Bus("<color>", "<fuel_type>", "<passengers>")
+print(car1)
+print(bus1)
 
 #4
-def sum_up(num1, num2):
-    sum = 0
-    for i in range (num1, num2 +1):
-        sum += i
-    if num2 > num1:
-        return sum
-    else:
-        return 0
-    if (num1 or num2) > 0:
-        return sum
-    else:
-        return 0
-print (sum_up(3,9))
+class Book:
+    def __init__(self, name, author):
+        self.name = name
+        self.author = author
+
+    def print_book(self):
+        print("{0},{1}".format(self.name,self.author))
+book1 = Book("Dune","Frank Herbert")
+book1.print_book()
 
 #5
-def circle_area(r1,r2,r3):
-    pi = 3.14
-    r1 = float(pi * r1 ** 2)
-    r2 = float(pi * r2 ** 2)
-    r3 = float(pi * r3 ** 2)
+from collections import namedtuple
+class BookShelf(object):
+   def __init__(self):
+       self.books = []
+   def add_book_list(self,book):
 
-    return r1+r2+r3
-i1 = circle_area(3,1,4)
-print(i1)
+           if isinstance(book, Book):
 
-#6
-def check_string(text):
-    text2 = text.lower()
-    if text2.startswith("a"):
-        return True
-    elif text2.endswith("a"):
-        return True
-    else:
-        return False
-print(check_string("nn"))
+              self.books.append(book)
 
-#7
-def triangle(rows):
-    for i in range (0 + rows):
-        for j in range (0, i+1):
-            print("* ", end ="")
-        print ("\r")
-rows = 4
-triangle(rows)
+   def books_by_author(self,author):
+       written_by_author = []
+       for book in self.books:
+           if book.author == author:
+               written_by_author.append(book.name)
+       return written_by_author
+   def get_books(self):
+       book_list = []
+       for book in self.books:
+           book_list.append(book.name)
+       return book_list
+   def clear_shelf(self):
+       self.books.clear()
+       return self.books
 
+Book = namedtuple("Book","name,author")
 
+n = BookShelf()
+n.add_book_list(Book("Geometry", "Potter"))
+n.add_book_list(Book("Math", "Harry"))
+n.add_book_list(Book("English", "James Odd"))
+o = n.books_by_author("Potter")
+print(o)
+g=n.get_books()
+print(g)
+c = n.clear_shelf()
+print(c)
 
-
-
-
+o = n.books_by_author("Potter")
+print(o)
